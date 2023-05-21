cdcc67e48a4e7aa883b9a2930c482be686af3777
Andrew Virts <asvirts@outlook.com>
2023-05-21 11:05:44 -0500

    Coffee

---
+++ Python Programming/Exercises/coffee.py
@@ -0,0 +1,6 @@
+# Make a cup of coffee
+
+print("Welcome to the Coffee Brand Name Generator")
+color = input("What is your favorite color?\n")
+animal = input("What is the name of your favorite animal?\n")
+print("Your new coffee brand name is " + str(color) + str(" ") + str(animal))
