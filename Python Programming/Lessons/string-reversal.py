901bc59c74ec996ab85b61fb2ff68bedda579dfb
Andrew Virts <asvirts@outlook.com>
2023-05-25 10:38:22 -0400

    Reversing a string on Python: Recursion

--- Python Programming/Lessons/string-reversal.py
+++ Python Programming/Lessons/string-reversal.py
@@ -1,5 +1,19 @@
-# str[start:stop:step]
+# Slice function
 
 trial = "reverse this string"
 new_trial = trial[::-1]
 print(new_trial)
+
+
+# Recursion
+
+def string_reversal(str):
+    if len(str) == 0:
+        return str
+    else:
+        return string_reversal(str[1:]) + str[0]
+
+
+str = "reverse me"
+reverse = string_reversal(str)
+print(reverse)
