41d9a3197611783154ec1da3210132dc5ce89cb2
Andrew Virts <asvirts@outlook.com>
2023-06-10 19:37:02 -0500

    Lesson: Writing import statements

--- Python Programming/Lessons/import-statement.py
+++ Python Programming/Lessons/import-statement.py
@@ -1,2 +1,17 @@
 import numpy
 import pandas
+
+from math import sqrt, factorial, log10
+import math as m
+
+root = sqrt(9)
+print(root)
+
+cosine = m.cos(0)
+print(cosine)
+
+log = log10(50)
+print(log)
+
+fac = factorial(96)
+print(fac)
