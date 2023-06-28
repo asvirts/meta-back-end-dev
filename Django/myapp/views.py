2bc87a1df9047bb9ab0876aae8e2253d09f0d332
Andrew Virts <asvirts@outlook.com>
2023-06-28 11:34:11 -0500

    Lesson: Creating Requests and Responses

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    path = request.path
+    user_agent = request.META['HTTP_USER_AGENT']
+    return HttpResponse(path, context_type='text/html', charset='utf-8', ua=user_agent)
