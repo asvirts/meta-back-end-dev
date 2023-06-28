a7decba637470699b314af9a57373dab0695d207
Andrew Virts <asvirts@outlook.com>
2023-06-28 15:04:11 -0500

    Lesson

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -12,3 +12,8 @@ def account(request):
     path = request.path
     user_agent = request.META['HTTP_USER_AGENT']
     return HttpResponse(path, context_type='text/html', charset='utf-8', ua=user_agent)
+
+def qryview (request):
+    name = request.GET['name']
+    id = request.GET['id'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
--- Django/urls.py
+++ Django/urls.py
@@ -5,4 +5,5 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'), views.home, name='Home'),
+    path('getuser/', views.qryview, name='qryview')
 ]
