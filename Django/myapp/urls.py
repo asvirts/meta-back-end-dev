f162b925333753ced8c930d64651d6a61fdeec77
Andrew Virts <asvirts@outlook.com>
2023-06-29 09:45:21 -0500

    Lesson: Mapping URLs with Params

--- Django/myapp/urls.py
+++ Django/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('/account', views.account)
+    path('/account', views.account),
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
 ]
--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -3,17 +3,30 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     path = request.path
     user_agent = request.META['HTTP_USER_AGENT']
     return HttpResponse(path, context_type='text/html', charset='utf-8', ua=user_agent)
+
 
 def account(request):
     path = request.path
     user_agent = request.META['HTTP_USER_AGENT']
     return HttpResponse(path, context_type='text/html', charset='utf-8', ua=user_agent)
 
-def qryview (request):
+
+def qryview(request):
     name = request.GET['name']
-    id = request.GET['id'] 
-    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+    id = request.GET['id']
+    return HttpResponse("Name:{} UserID:{}".format(name, id))
+
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of hot beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
